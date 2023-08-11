0efd786ba12d0fe204059a1b4b335916bf102c1f
= <=>
2023-08-11 20:28:01 +0530

    update v1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,39 @@
 
-import React from "react";
+
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <ParentComponent />
     </div>
   )
 }
 
-export default App
+const ParentComponent = () => {
+
+  let [inputValue, setInputValue] = useState("")
+  return (
+    <div className="parent">
+      <h1>Parent Component</h1>
+      {inputValue ? <p>{inputValue}</p> : null}
+      <ChildComponent inputValue={inputValue} setInputValue={setInputValue} />
+    </div>
+  )
+}
+
+const ChildComponent = ( {inputValue, setInputValue}) => {
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  }
+  return (
+    <div className="child">
+      <h1>Child Component</h1>
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+    </div>
+  )
+}
+
+export default App;
